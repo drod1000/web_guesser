9558c01d1163632139d3c579faf5ed290f77f45e
Daniel Rodriguez <drod2793@gmail.com>
2016-11-16 08:51:51 -0700

    Extension: cheat mode completed

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,10 @@ NUMBER = rand(100)
 
 get '/' do
   guess = params['guess']
+  cheat = params['cheat']
   message = check_guess(guess)
-  erb :index, :locals => {:number => NUMBER, :guess => guess, :message => message}
+  message << display_number if cheat == "true"
+  erb :index, :locals => {:number => NUMBER, :message => message}
 end
 
 def check_guess(guess)
@@ -14,5 +16,9 @@ def check_guess(guess)
   return "Too high!" if guess.to_i > NUMBER
   return "Way too low!" if guess.to_i < NUMBER - 5
   return "Too low!" if guess.to_i < NUMBER
-  "You got it right! The secret number is #{NUMBER}"
+  "You got it right! #{display_number}"
+end
+
+def display_number
+  "The secret number is #{NUMBER}"
 end
